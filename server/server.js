67f0e0c02a37755a15da99b6e6a8bb0ecea5029f
Mike <84146116+Mike5535@users.noreply.github.com>
2022-09-24 13:20:38 +0300

    remove_unused

--- server/server.js
+++ server/server.js
@@ -1,18 +1,11 @@
 'use strict';
 
 const express = require('express');
-const body = require('body-parser');
-const cookie = require('cookie-parser');
-const morgan = require('morgan');
-const uuid = require('uuid').v4;
 const path = require('path');
 const app = express();
 
-app.use(morgan('dev'));
 app.use(express.static(path.resolve(__dirname, '..','public')));
 app.use(express.static(path.resolve(__dirname, 'images')));
-app.use(body.json());
-app.use(cookie());
 
 const port = process.env.PORT || 3000;
 
