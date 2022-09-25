8e4000d77f5630ad50def4a88bb746721ec7bd85
Ivan Biryukov <vanyabiryukov@yandex.ru>
2022-09-25 17:28:34 +0300

    add prototype content_elem, film, hover film

--- public/index.js
+++ public/index.js
@@ -1,6 +1,34 @@
-let template = Handlebars.templates['navbar']
-let navbar = template({})
-console.log(navbar)
 
-document.body.innerHTML = navbar;
-const root = document.getElementById('root');
+// addElementFromTemplate(root, 'navbar', {})
+// addElementFromTemplate(header, 'bigContent', {})
+
+// let main = document.createElement('')
+let root = document.getElementById('root')
+
+let navbar = Handlebars.templates['navbar']({})
+let contentBig = Handlebars.templates['bigContent']({})
+
+let div = document.createElement('div')
+
+// div.dataset.backgroundBigContent = 'img/dune_back.jpg'
+
+div.innerHTML = navbar
+root.append(div)
+
+div = document.createElement('div')
+div.innerHTML = contentBig
+// root.append(div) +++
+
+
+// function addElementFromTemplate(elementDOM, templateName, context) {
+//     // let renderTemplate = Handlebars.templates[templateName]
+//     // let html = renderTemplate(context)
+//     let html = Handlebars.templates[templateName](context)
+//     let div = document.createElement('div')
+//     // const sanitizer = new Sanitizer();
+//     // div.setHTML(html, {sanitizer})
+//     div.innerHTML = html
+
+//     // const elem = document.getElementById(element);
+//     elementDOM.append(div);
+// }
