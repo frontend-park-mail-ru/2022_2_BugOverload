6ad9ea379063859599f908dbede294ec054dcba6
Ivan Biryukov <vanyabiryukov@yandex.ru>
2022-09-26 23:25:37 +0300

    TP-1f6 add: tmpls for preview, collections

--- public/index.js
+++ public/index.js
@@ -3,22 +3,138 @@
 // addElementFromTemplate(header, 'bigContent', {})
 
 // let main = document.createElement('')
+
+const collectionCinemaTodayData = {
+    tittle: "Сейчас в кино",
+    films:[
+    {
+        tittle: "Дюна",
+        poster: "img/posters/dune_poster.jpg",
+        rating: 7.1,
+        genrys: "Фентези, Приключения",
+        year: 2015,
+        href: "index.html",
+    },
+    {
+        tittle: "Дюна",
+        poster: "img/posters/1.png",
+        rating: 7.3,
+        genrys: "Фентези, Приключения",
+        year: 2015,
+        href: "index.html",
+    },
+    {
+        tittle: "Дюна",
+        poster: "img/posters/2.png",
+        rating: 7.4,
+        genrys: "Фентези, Приключения",
+        year: 2015,
+        href: "index.html",
+    },
+    {
+        tittle: "Дюна",
+        poster: "img/posters/3.png",
+        rating: 7.5,
+        genrys: "Фентези, Приключения",
+        year: 2015,
+        href: "index.html",
+    },
+    {
+        tittle: "Дюна",
+        poster: "img/posters/4.png",
+        rating: 7.9,
+        genrys: "Фентези, Приключения",
+        year: 2015,
+        href: "index.html",
+    },
+    {
+        tittle: "Дюна",
+        poster: "img/posters/5.png",
+        rating: 7.2,
+        genrys: "Фентези, Приключения",
+        year: 2015,
+        href: "index.html",
+    },
+]}
+
+
+const collectionPopularData = {films:[
+    {
+        tittle: "Дюна",
+        poster: "dune_poster.jpg",
+        rating: 7.1,
+        genrys: ["Фентези", "Приключения"],
+        year: 2015
+    },
+    {
+        tittle: "Дюна",
+        poster: "1.png",
+        rating: 7.2,
+        genrys: ["Фентези", "Приключения"],
+        year: 2015
+    },
+    {
+        tittle: "Дюна",
+        poster: "2.png",
+        rating: 7.2,
+        genrys: ["Фентези", "Приключения"],
+        year: 2015
+    },
+    {
+        tittle: "Дюна",
+        poster: "3.png",
+        rating: 7.2,
+        genrys: ["Фентези", "Приключения"],
+        year: 2015
+    },
+    {
+        tittle: "Дюна",
+        poster: "4.png",
+        rating: 7.2,
+        genrys: ["Фентези", "Приключения"],
+        year: 2015
+    },
+    {
+        tittle: "Дюна",
+        poster: "5.png",
+        rating: 7.2,
+        genrys: ["Фентези", "Приключения"],
+        year: 2015
+    },
+]}
+
 let root = document.getElementById('root')
 
 let navbar = Handlebars.templates['navbar']({})
-let contentBig = Handlebars.templates['bigContent']({})
-
 let div = document.createElement('div')
-
-// div.dataset.backgroundBigContent = 'img/dune_back.jpg'
-
 div.innerHTML = navbar
+
 root.append(div)
 
+let previewFilm = Handlebars.templates['previewFilm']({})
 div = document.createElement('div')
-div.innerHTML = contentBig
-// root.append(div) +++
+div.innerHTML = previewFilm
 
+root.append(div)
+
+let collectionCinemaToday = Handlebars.templates['collection'](collectionCinemaTodayData)
+div = document.createElement('div')
+div.innerHTML = collectionCinemaToday
+
+root.append(div)
+
+// let collectionPopular = Handlebars.templates['collection']({})
+// div = document.createElement('div')
+// div.innerHTML = collectionPopular
+
+// root.append(div)
+
+// function renderTemplate(templateName, callback, parametrs = {}) {
+//     const template = Handlebars.templates[templateName];
+//     const templateHtml = template(parametrs);
+//     const safeheaderHtml = DOMPurify.sanitize(templateHtml);
+//     callback(safeheaderHtml);
+// }
 
 // function addElementFromTemplate(elementDOM, templateName, context) {
 //     // let renderTemplate = Handlebars.templates[templateName]
