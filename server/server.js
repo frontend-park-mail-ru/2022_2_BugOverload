562d7f7097ce9363eb3c5ae694e0c70d14e8f0e9
Ivan Biryukov <vanyabiryukov@yandex.ru>
2022-09-29 22:29:01 +0300

    TP-7f1 add: linter airbnb, rules, eslintignore, lint script

--- server/server.js
+++ server/server.js
@@ -1,14 +1,13 @@
-'use strict';
-
 const express = require('express');
 const path = require('path');
+
 const app = express();
 
-app.use(express.static(path.resolve(__dirname, '..','public')));
+app.use(express.static(path.resolve(__dirname, '..', 'public')));
 app.use(express.static(path.resolve(__dirname, 'images')));
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, function () {
-	console.log(`Server listening port ${port}`);
+app.listen(port, () => {
+    console.log(`Server listening port ${port}`);
 });
---
+++ src/index.js
@@ -0,0 +1,2 @@
+const a = 10;
+console.log(a);
