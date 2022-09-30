54fcc8bcf146366412317f9fb82a2a18f99d3f37
Mike5535 <volkovnhk@mail.ru>
2022-09-30 17:23:15 +0300

    add_cors

--- src/utils/ajax.js
+++ src/utils/ajax.js
@@ -1,6 +1,9 @@
 export class Ajax {
     get = async ({url,callback}) => {
-        let response = await fetch(url);
+        let response = await fetch(url, {
+            mode: 'cors',
+            credentials: 'include',
+        });
         let result = await response.json();
         callback(response, result);
     }
@@ -9,6 +12,8 @@ export class Ajax {
         let response = await fetch(
             url, {
                 method: 'POST',
+                mode: 'cors',
+                credentials: 'include',
                 headers: {
                     'Content-Type': 'application/json;charset=utf-8'
                   },
