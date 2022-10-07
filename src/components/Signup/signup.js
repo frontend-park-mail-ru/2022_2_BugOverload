8650c887b8593030517d6c6bb9442d39186cd852
Mike5535 <volkovnhk@mail.ru>
2022-10-07 16:31:40 +0300

    fix-links

--- src/components/Signup/signup.js
+++ src/components/Signup/signup.js
@@ -29,7 +29,7 @@ export class Signup {
      */
     postRequestData(user) {
         const responsePromise = Ajax.post({
-            url: 'http://movie-gate.online/v1/auth/signup',
+            url: 'http://movie-gate.online:8088/v1/auth/signup',
             body: user,
         });
 
