e397b9701e085a13abca70d03140904069f6b32d
Mike5535 <volkovnhk@mail.ru>
2022-11-09 23:04:06 +0300

    delete-comments

--- src/sw.js
+++ src/sw.js
@@ -74,37 +74,3 @@ async function networkFirst(request) {
         return cached;
     }
 }
-/*
-const CACHE_NAME = 'moviegate-v-1';
-
-// const assetUrls = [];
-
-// this.addEventListener('install', async () => {
-//     const cache = await caches.open(CACHE_NAME);
-//     await cache.addAll(assetUrls);
-// });
-
-// this.addEventListener('fetch', (event) => {
-//     const { request } = event;
-
-//     event.respondWith((async () => {
-//         if (navigator.onLine) {
-//             const response = await fetch(request);
-//             if (request.method !== 'GET') {
-//                 return response;
-//             }
-
-//             const clone = response.clone();
-//             caches.open(CACHE_NAME)
-//                 .then((cache) => {
-//                     cache.put(request, clone);
-//                 });
-//             return response;
-//         }
-
-//         const response = await caches.match(request);
-
-       // undefined или данные из кэша
-       return response;
-   })());
-}); */
