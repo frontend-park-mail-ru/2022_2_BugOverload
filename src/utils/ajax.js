4e84965b1e7700f08aba274a820ae61e2b71fed7
Mike5535 <volkovnhk@mail.ru>
2022-11-10 16:53:48 +0300

    has

--- src/utils/ajax.js
+++ src/utils/ajax.js
@@ -23,6 +23,8 @@ export class Ajax {
             },
         });
         const csrf = response.headers.get('x-csrf-token');
+        const csrfhas = response.headers.has('X-CSRF-TOKEN');
+        console.log(csrfhas)
         console.log('getCsrf', csrf, response.headers.get('X-CSRF-TOKEN'),response.headers.forEach(console.log))
         if (csrf) {
             this.#csrfToken = csrf;
