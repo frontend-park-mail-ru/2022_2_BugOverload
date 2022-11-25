1334198e751ace0885aa310cff6fdd67634bdf04
Ivan Biryukov <vanyabiryukov@yandex.ru>
2022-11-26 01:09:56 +0300

    add mock

--- src/store/reducers/mockData.ts
+++ src/store/reducers/mockData.ts
@@ -174,3 +174,145 @@ export const mockPremieres = () => ({
     ],
     'name': 'Сейчас в кино'
 });
+export const mockSearch = () => ({
+    films: [{
+        description: "Британская лингвистка Алетея прилетает из Лондона",
+        directors: [
+          {
+            id: 123123,
+            name: "Стивен Спилберг"
+          }
+        ],
+        duration_minutes: 55,
+        genres: [
+          "фэнтези",
+          "приключения"
+        ],
+        id: 23,
+        name: "Game of Thrones s.1",
+        poster_ver: "23",
+        prod_countries: [
+          "США",
+          "Великобритания"
+        ],
+        prod_date: "2014.01.13",
+        rating: 9.2
+      }, {
+        description: "Британская лингвистка Алетея прилетает из Лондона",
+        directors: [
+          {
+            id: 123123,
+            name: "Стивен Спилберг"
+          }
+        ],
+        duration_minutes: 55,
+        genres: [
+          "фэнтези",
+          "приключения"
+        ],
+        id: 24,
+        name: "Game of Thrones s.2",
+        poster_ver: "24",
+        prod_countries: [
+          "США",
+          "Великобритания"
+        ],
+        prod_date: "2014.01.13",
+        rating: 9.2
+      }],
+    serials: [{
+        description: "Британская лингвистка Алетея прилетает из Лондона",
+        directors: [
+          {
+            id: 123123,
+            name: "Стивен Спилберг"
+          }
+        ],
+        duration_minutes: 55,
+        genres: [
+          "фэнтези",
+          "приключения"
+        ],
+        id: 25,
+        name: "Game of Thrones s.3",
+        poster_ver: "25",
+        prod_countries: [
+          "США",
+          "Великобритания"
+        ],
+        prod_date: "2014.01.13",
+        rating: 9.2
+      }, {
+        description: "Британская лингвистка Алетея прилетает из Лондона",
+        directors: [
+          {
+            id: 123123,
+            name: "Стивен Спилберг"
+          }
+        ],
+        duration_minutes: 55,
+        genres: [
+          "фэнтези",
+          "приключения"
+        ],
+        id: 26,
+        name: "Game of Thrones s.4",
+        poster_ver: "26",
+        prod_countries: [
+          "США",
+          "Великобритания"
+        ],
+        prod_date: "2014.01.13",
+        rating: 9.2
+      }],
+    persons: [
+        {
+            id: 27,
+            name: 'Василий Петров',
+            avatar: '27',
+            birthday: '1978.02.21',
+            professions: [
+                "актер",
+                "продюсер",
+                "режиссер"
+            ],
+            count_films: 130,
+        },
+        {
+            id: 30,
+            name: 'Кристина Асмус',
+            avatar: '30',
+            birthday: '1986.02.21',
+            professions: [
+                "актер",
+                "продюсер",
+                "режиссер"
+            ],
+            count_films: 322,
+        },
+        {
+            id: 12,
+            name: 'Светлана Хотченкова',
+            avatar: '12',
+            birthday: '1991.02.21',
+            professions: [
+                "актер",
+                "продюсер",
+                "режиссер"
+            ],
+            count_films: 112,
+        },
+        {
+            id: 26,
+            name: 'Фёдор Добронравов',
+            avatar: '26',
+            birthday: '1938.02.21',
+            professions: [
+                "актер",
+                "продюсер",
+                "Пьяница"
+            ],
+            count_films: 11,
+        }
+    ],
+});
--- src/store/reducers/reducerCommon.ts
+++ src/store/reducers/reducerCommon.ts
@@ -1,6 +1,6 @@
 import { Ajax } from '@utils/ajax';
 import { API, responsStatuses } from '@config/config';
-import { mockCollection, mockPrewiew } from '@store/reducers/mockData';
+import { mockCollection, mockPrewiew, mockSearch } from '@store/reducers/mockData';
 
 class ReducerCommon {
     async getCollectionData({name, target, key, sortParam, countFilms, delimiter} :collectionParams) {
@@ -33,159 +33,24 @@ class ReducerCommon {
     }
 
     async getSearchData({request}: searchParams) {
-        // const response = await Ajax.get(API.search(request)) as Response;
-        // console.log(`responseStatus ${response.status} ${request}`);
-        // if (response.status === responsStatuses.OK) {
-        //     return { search: response.body };
-        // }
+        let response;
+        try {
+            response = await Ajax.get(API.search(request)) as Response;
+        } catch (e) {
+            if (request === 'q-qwe') {
+                return { search: mockSearch() };
+            }
+            return { search: { error: 'error' } };
+        }
+        console.log(`responseStatus ${response.status} ${request}`);
+        if (response.status === responsStatuses.OK) {
+            return { search: response.body };
+        }
 
         // if (response.status === responsStatuses.NotFound) {
-          if (request === 'q-qwe') {
-            return { search:
-                {
-                    // films: [{
-                    //     description: "Британская лингвистка Алетея прилетает из Лондона",
-                    //     directors: [
-                    //       {
-                    //         id: 123123,
-                    //         name: "Стивен Спилберг"
-                    //       }
-                    //     ],
-                    //     duration_minutes: 55,
-                    //     genres: [
-                    //       "фэнтези",
-                    //       "приключения"
-                    //     ],
-                    //     id: 23,
-                    //     name: "Game of Thrones s.1",
-                    //     poster_ver: "23",
-                    //     prod_countries: [
-                    //       "США",
-                    //       "Великобритания"
-                    //     ],
-                    //     prod_date: "2014.01.13",
-                    //     rating: 9.2
-                    //   }, {
-                    //     description: "Британская лингвистка Алетея прилетает из Лондона",
-                    //     directors: [
-                    //       {
-                    //         id: 123123,
-                    //         name: "Стивен Спилберг"
-                    //       }
-                    //     ],
-                    //     duration_minutes: 55,
-                    //     genres: [
-                    //       "фэнтези",
-                    //       "приключения"
-                    //     ],
-                    //     id: 24,
-                    //     name: "Game of Thrones s.2",
-                    //     poster_ver: "24",
-                    //     prod_countries: [
-                    //       "США",
-                    //       "Великобритания"
-                    //     ],
-                    //     prod_date: "2014.01.13",
-                    //     rating: 9.2
-                    //   }],
-                    serials: [{
-                        description: "Британская лингвистка Алетея прилетает из Лондона",
-                        directors: [
-                          {
-                            id: 123123,
-                            name: "Стивен Спилберг"
-                          }
-                        ],
-                        duration_minutes: 55,
-                        genres: [
-                          "фэнтези",
-                          "приключения"
-                        ],
-                        id: 25,
-                        name: "Game of Thrones s.3",
-                        poster_ver: "25",
-                        prod_countries: [
-                          "США",
-                          "Великобритания"
-                        ],
-                        prod_date: "2014.01.13",
-                        rating: 9.2
-                      }, {
-                        description: "Британская лингвистка Алетея прилетает из Лондона",
-                        directors: [
-                          {
-                            id: 123123,
-                            name: "Стивен Спилберг"
-                          }
-                        ],
-                        duration_minutes: 55,
-                        genres: [
-                          "фэнтези",
-                          "приключения"
-                        ],
-                        id: 26,
-                        name: "Game of Thrones s.4",
-                        poster_ver: "26",
-                        prod_countries: [
-                          "США",
-                          "Великобритания"
-                        ],
-                        prod_date: "2014.01.13",
-                        rating: 9.2
-                      }],
-                    persons: [
-                        {
-                            id: 27,
-                            name: 'Василий Петров',
-                            avatar: '27',
-                            birthday: '1978.02.21',
-                            professions: [
-                                "актер",
-                                "продюсер",
-                                "режиссер"
-                            ],
-                            count_films: 130,
-                        },
-                        {
-                            id: 30,
-                            name: 'Кристина Асмус',
-                            avatar: '30',
-                            birthday: '1986.02.21',
-                            professions: [
-                                "актер",
-                                "продюсер",
-                                "режиссер"
-                            ],
-                            count_films: 322,
-                        },
-                        {
-                            id: 12,
-                            name: 'Светлана Хотченкова',
-                            avatar: '12',
-                            birthday: '1991.02.21',
-                            professions: [
-                                "актер",
-                                "продюсер",
-                                "режиссер"
-                            ],
-                            count_films: 112,
-                        },
-                        {
-                            id: 26,
-                            name: 'Фёдор Добронравов',
-                            avatar: '26',
-                            birthday: '1938.02.21',
-                            professions: [
-                                "актер",
-                                "продюсер",
-                                "Пьяница"
-                            ],
-                            count_films: 11,
-                        }
-                    ],
-                }
-            }
-          }
+          // if (request === 'q-qwe') {
+          //   return { search:}
+          // }
         // }
         console.log(`getSearchData : nothing`)
         return { search: { error: 'error' } };
