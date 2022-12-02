078342e6e4000e1c4b6316a182bee02e88e10afb
Mike5535 <volkovnhk@mail.ru>
2022-12-02 17:57:37 +0300

    change v cashe

--- src/sw.js
+++ src/sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'moviegate-v-9';
-const DYNAMIC_CACHE_NAME = 'd-moviegate-v-9';
+const CACHE_NAME = 'moviegate-v-10';
+const DYNAMIC_CACHE_NAME = 'd-moviegate-v-10';
 
 const whiteDynamicUrls = [
     '/film/',
