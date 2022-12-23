4bb4da471a9a6e0ed03869631a11b046d1596894
Mike5535 <volkovnhk@mail.ru>
2022-12-23 19:48:45 +0300

    check

--- src/components/Login/login.ts
+++ src/components/Login/login.ts
@@ -37,7 +37,10 @@ export class Login extends Component {
         };
 
         this.subscribeLoginpStatus = this.subscribeLoginpStatus.bind(this);
-        this.subscribeLogin = this.subscribeLogin.bind(this);
+        this.subscribeLogin = () => {
+            console.log(this)
+            this.render();
+        };
     }
 
     /**
@@ -195,10 +198,6 @@ export class Login extends Component {
      */
     subscribeLoginpStatus() {
         this.state.statusLogin = store.getState('statusLogin');
-        this.render();
-    }
-
-    subscribeLogin() {
         this.render();
     }
 }
