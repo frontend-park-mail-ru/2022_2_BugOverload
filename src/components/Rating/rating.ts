8c2a885c46da587f01cd5a1df4d7763097110a39
Mike5535 <volkovnhk@mail.ru>
2022-12-24 00:44:11 +0300

    check rating

--- src/components/Rating/rating.ts
+++ src/components/Rating/rating.ts
@@ -33,6 +33,7 @@ export class Rating extends Component {
             this.state.countScores = store.getState('countScores') || props.film.count_ratings;
 
             this.render();
+            store.dispatch(actionGetMetaDataFilm({ filmID: this.state.film.id }));
         };
 
         store.subscribe('rating', this.subHandlerRating);
@@ -166,3 +167,4 @@ export class Rating extends Component {
         store.unsubscribe('statusRating', this.subHandlerStatusRating);
     }
 }
+ 
