9831dbb04e93bfae3414e634a85f840101e4fde1
Mike5535 <volkovnhk@mail.ru>
2022-12-24 12:43:17 +0300

    fix

--- src/components/DetailsFilm/detailsFilm.ts
+++ src/components/DetailsFilm/detailsFilm.ts
@@ -14,18 +14,19 @@ export class DetailsFilm extends Component {
     constructor(information: fullDetails) {
         super(); 
         this.information = information;
-        if (this.information.actors) {
-            this.information.actors
-                .forEach((person: actor) => { person.avatar = API.img.person_avatar(person.avatar); });
-        }
-        /*console.log('this.information',this.informations )
+        // if (this.information.actors) {
+        //     this.information.actors
+        //         .forEach((person: actor) => { person.avatar = API.img.person_avatar(person.avatar); });
+        // }
+        console.log('this.information',this.informations )
         if (this.information.actors) {
             this.information.actors = this.information.actors.map(
                 (person: actor) => { 
-                    API.img.person_avatar(person.avatar); 
+                    person.avatar = API.img.person_avatar(person.avatar); 
+                    return person;
                 }
             );
-        }*/
+        }
         console.log('this.information after',this.information)
         this.location = document.querySelector('.js-film-page__details');
     }
