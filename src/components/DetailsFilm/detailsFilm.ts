b6ed8cd7690f1b0ebd412307c3d7fbda061c8c89
Mike5535 <volkovnhk@mail.ru>
2022-12-24 12:04:26 +0300

    check

--- src/components/DetailsFilm/detailsFilm.ts
+++ src/components/DetailsFilm/detailsFilm.ts
@@ -14,18 +14,18 @@ export class DetailsFilm extends Component {
     constructor(information: fullDetails) {
         super(); 
         this.information = information;
-       /* if (this.information.actors) {
+        if (this.information.actors) {
             this.information.actors
                 .forEach((person: actor) => { person.avatar = API.img.person_avatar(person.avatar); });
-        }*/
-        console.log('this.information',this.informations )
+        }
+        /*console.log('this.information',this.informations )
         if (this.information.actors) {
             this.information.actors = this.information.actors.map(
                 (person: actor) => { 
                     API.img.person_avatar(person.avatar); 
                 }
             );
-        }
+        }*/
         console.log('this.information after',this.information)
         this.location = document.querySelector('.js-film-page__details');
     }
